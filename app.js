ccd09fc43c49065394d6176bf86c6977c9a30c0c
sdoire <sarah.doire@gmail.com>
2015-07-25 14:56:33 -0500

    hard and pro mode additions

--- app.js
+++ app.js
@@ -9,26 +9,49 @@ $(document).ready(function(){
 		
 	
 		search($input);
+		search2($input);
 	});
 
 
 });
 
 function callback(data){
-    	console.log(data.login);
-    	var URL = encodeURI(data.html_url);
-    	$(".search").append("<div><h3 class='info'>" + "Username: " + data.login + "</h3>" + "<br>" + "<div> <a class='link' href=\"" + URL + "\">Github Page</a> </div>");
+    	console.log(data);
+    	$(".search").append("<div class='info'><h3>" + "Username: " + data.login + "</h3>" + "<br>" + "<a class='link' href=\"" + data.html_url + "\">Github Page</a> </div>");
         $(".search").append("<div>" + "<img class='img' src=\"" + data.avatar_url + "\" \/>" + "</div>");
+}
+function repoCallback(data){
+    	console.log(data);
+    	for (var i = 0; i<data.length; i++) {
+    	$(".search").append("<div class='repo'><h3 class='info'>" + "Reponame: " + data[i].name + "</h3>" + "<br>" + "<a class='link' href=\"" + data[i].html_url + "\">Repo Link</a> </div>");
+    }
 }
 
 function search (query) {
 	$.ajax({
-		url: 'https://api.github.com/users/' + $input + '?client_id=f8a4b95805c9804c9eb7&client_secret=4b1bff35a5b8b802fe4bb4e1204afd2f56fc8d8d',
+		url: 'https://api.github.com/users/' + encodeURI($input) + '?client_id=f8a4b95805c9804c9eb7&client_secret=4b1bff35a5b8b802fe4bb4e1204afd2f56fc8d8d',
 		type: 'GET',
 		crossDomain: true,
 		dataType: 'json',
 		success: function(data){
 			callback(data);
+		},
+		error: function(errorThrown) { 
+            $(".search").append("<p>" + "User not found" + "</p>");
+        },
+        complete: function() {
+	        console.log('ajax complete');
+	    }
+	});
+}
+function search2 (query2) {
+	$.ajax({
+		url: 'https://api.github.com/users/' + encodeURI($input) + '/repos?client_id=f8a4b95805c9804c9eb7&client_secret=4b1bff35a5b8b802fe4bb4e1204afd2f56fc8d8d',
+		type: 'GET',
+		crossDomain: true,
+		dataType: 'json',
+		success: function(data){
+			repoCallback(data);
 		},
 		error: function(errorThrown) { 
             $(".search").append("<p>" + "User not found" + "</p>");
